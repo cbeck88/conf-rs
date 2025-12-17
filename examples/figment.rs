af2f142e9afb36b15e8c5b383fcf8d2f488f7cdb
Chris Beck <beck.ct@gmail.com>
2025-12-16 18:13:08 -0700

    use required-features pattern to cleanup serde examples

--- examples/figment.rs
+++ examples/figment.rs
@@ -1,13 +1,40 @@
-#[cfg(feature = "serde")]
-#[path = "serde/figment.rs"]
-mod serde_figment;
+use conf::Conf;
+use figment::{
+    Figment,
+    providers::{Format, Json, Toml},
+    value::Value,
+};
+use std::env;
 
-#[cfg(feature = "serde")]
-fn main() {
-    serde_figment::main()
-}
+#[path = "./common/model_service.rs"]
+mod model_service;
+use model_service::ModelServiceConfig;
 
-#[cfg(not(feature = "serde"))]
 fn main() {
-    panic!("needs serde feature")
+    // In this example, we show how figment can be used together with conf
+    // in order to load content from multiple files, merge them according to
+    // some hierarchical order, and then supply the result to conf.
+    //
+    // This takes advantage of conf's comprehensive error reporting, and gives
+    // better results than if you just extract() directly into your final
+    // config structure.
+    let mut fig = Figment::new();
+
+    if let Some(path) = env::var_os("TOML") {
+        fig = fig.merge(Toml::file(path));
+    }
+    if let Some(path) = env::var_os("TOML2") {
+        fig = fig.merge(Toml::file(path));
+    }
+    if let Some(path) = env::var_os("JSON") {
+        fig = fig.merge(Json::file(path));
+    }
+
+    let doc_content: Value = fig.extract().unwrap();
+
+    let config = ModelServiceConfig::conf_builder()
+        .doc("files", &doc_content)
+        .parse();
+
+    println!("{config:#?}");
 }
